49c980282526681cee95bbf4fcc7b4044da84ff0
Mezisashe Ojuba <mezisashe.ojuba@bison.howard.edu>
2024-09-29 05:48:51 +0000

    changed handler function name to alarm_handler, added time stamps and counter

--- timer.c
+++ timer.c
@@ -6,11 +6,11 @@
 
 int print_message = 0;
 
-void handler(int signum)
-{ //signal handler
+void alarm_handler(int signum)
+{ //signal alarm_handler
   printf("Hello World!\n");
   print_message = 1;
-  // signal(SIGALRM, handler); // he handler automatically disables handling of future signals so we set it again here.
+  // signal(SIGALRM, alarm_handler); // he alarm_handler automatically disables handling of future signals so we set it again here.
 }
 
 int main(int argc, char * argv[])
@@ -19,11 +19,15 @@ int main(int argc, char * argv[])
   idea: 
   get current time at beginning of main
   set counter for number of SIGALRM that has passed
-  in handler for ctrl + c, get current time
+  in alarm_handler for ctrl + c, get current time
   subtract to get the time
   */
+  int alarms_delivered = 0;
+  time_t seconds;
+     
+  start = time(NULL);
 
-  signal(SIGALRM,handler); //register handler to handle SIGALRM
+  signal(SIGALRM,alarm_handler); //register alarm_handler to handle SIGALRM
   alarm(5);
   while(1){
     
@@ -31,6 +35,7 @@ int main(int argc, char * argv[])
       
       printf("Turing was right!\n");
       print_message = 0;
+      alarms_delivered ++;
       alarm(5);
     }
   
