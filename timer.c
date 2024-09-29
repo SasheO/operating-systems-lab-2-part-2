b2ce50e5fd86e7909c4631e6dcc839625a695d60
Mezisashe Ojuba <mezisashe.ojuba@bison.howard.edu>
2024-09-29 06:11:46 +0000

    completed timer.c

--- timer.c
+++ timer.c
@@ -5,12 +5,23 @@
 #include <unistd.h>
 
 int print_message = 0;
+int start;
+int alarms_delivered;
 
 void alarm_handler(int signum)
 { //signal alarm_handler
   printf("Hello World!\n");
   print_message = 1;
   // signal(SIGALRM, alarm_handler); // he alarm_handler automatically disables handling of future signals so we set it again here.
+}
+
+void int_handler(int signum)
+{ //signal alarm_handler
+  int current = time(NULL);
+  int time_in_execution = current-start;
+  printf("Number of alarms: %d\n", alarms_delivered);
+  printf("Time in time_in_execution: %d secs\n", time_in_execution);
+  exit(0);
 }
 
 int main(int argc, char * argv[])
@@ -22,12 +33,12 @@ int main(int argc, char * argv[])
   in alarm_handler for ctrl + c, get current time
   subtract to get the time
   */
-  int alarms_delivered = 0;
-  time_t seconds;
+  alarms_delivered = 0;
      
   start = time(NULL);
 
   signal(SIGALRM,alarm_handler); //register alarm_handler to handle SIGALRM
+  signal(SIGINT,int_handler); //register alarm_handler to handle SIGALRM
   alarm(5);
   while(1){
     
